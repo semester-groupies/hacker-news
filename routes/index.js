85b32ea54f3d3ca59c4c11f7702936e4896e28e7
unknown <athinodoros.sgouromallis@hotmail.com>
2017-09-19 14:40:06 +0200

    added excitement

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'HackerNews Clone Front-End ',groupName: "semester groupies" });
+  res.render('index', { title: 'HackerNews Clone Front-End!!!',groupName: "semester groupies" });
 });
 
 module.exports = router;
