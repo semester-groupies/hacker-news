dc31cacaec85e64f812cca677d8eb565c039f0ab
Martin Macej <martin@MacBook-Pro-uzivatela-Martin.local>
2017-10-30 10:18:28 +0100

    continous

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,18 +7,29 @@ export class ApiService {
   private URL = 'http://www.favl.dk:8080/';
 
   public createUser(userObject) {
-    //this.http.post('url', 'body', 'options');
+    // this.http.post('url', 'body', 'options');
     return this.http.post(this.URL + 'user/create', userObject);
   }
 
 
   public loginUser(userObject) {
-    //this.http.post('url', 'body', 'options');
+    // this.http.post('url', 'body', 'options');
     return this.http.post(this.URL + 'user/login', userObject);
   }
 
   public post(postObject) {
-    return this.http.post(this.URL + 'post', postObject);
+    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    const pObject = {
+      'username': currentUser.username,
+      'post_type': postObject['type'],
+      'pwd_hash': currentUser.password,
+      'post_title': postObject['title'],
+      'post_url': postObject['url'],
+      'post_parent': -1,
+      'hanesst_id': 21,
+      'post_text': postObject['text'],
+    };
+    return this.http.post(this.URL + 'post', pObject);
 
 
   }
